58b8b084d6a1d01c59ca0400ef332fd11edeeda9
Florimond Lalande <38161801+Flowpy@users.noreply.github.com>
2021-07-08 01:00:17 +0200

    Fix frame.json + regen cpp header.

--- cpp_header_gen/protocol.gen.hpp
+++ cpp_header_gen/protocol.gen.hpp
@@ -1,5 +1,3 @@
-#include <Arduino.h>
-
 // PROTOCOL (frame_id, param_length, [params sizes]) --> generated with Python
 const uint8_t PING[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 const uint8_t PONG[] = {1, 2, 1, 1, 0, 0, 0, 0, 0, 0};
@@ -12,13 +10,13 @@ const uint8_t ROTATE[] = {10, 1, 2, 0, 0, 0, 0, 0, 0, 0};
 const uint8_t SET_LEFT_PID[] = {12, 3, 2, 2, 2, 0, 0, 0, 0, 0};
 const uint8_t SET_RIGHT_PID[] = {13, 3, 2, 2, 2, 0, 0, 0, 0, 0};
 const uint8_t SET_BOTH_PID[] = {14, 3, 2, 2, 2, 0, 0, 0, 0, 0};
-const uint8_t SET_PWM[] = {15, 2, 2, 2, 0, 0, 0, 0, 0, 0};
+const uint8_t SET_PWM[] = {15, 3, 2, 2, 2, 0, 0, 0, 0, 0};
 const uint8_t SET_POSITION[] = {16, 3, 2, 2, 2, 0, 0, 0, 0, 0};
 const uint8_t SET_STM_PARAM[] = {17, 3, 2, 2, 2, 0, 0, 0, 0, 0};
 const uint8_t CURRENT_POS[] = {18, 3, 2, 2, 2, 0, 0, 0, 0, 0};
 const uint8_t CURRENT_PWM[] = {19, 2, 2, 2, 0, 0, 0, 0, 0, 0};
 const uint8_t CURRENT_SPEED[] = {20, 3, 2, 2, 2, 0, 0, 0, 0, 0};
-const uint8_t SONAR_DISTANCE[] = {23, 4, 1, 1, 1, 1, 0, 0, 0, 0};
+const uint8_t SONAR_DISTANCE[] = {23, 6, 1, 1, 1, 1, 1, 1, 0, 0};
 const uint8_t ORDER_COMPLETE[] = {26, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 const uint8_t ROBOT_BLOCKED[] = {28, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 const uint8_t UPDATE_SCREEN[] = {30, 2, 2, 1, 0, 0, 0, 0, 0, 0};
@@ -26,6 +24,10 @@ const uint8_t FETCH_PUCK[] = {40, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 const uint8_t PUCK_FETCHED_CHIEF[] = {41, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 const uint8_t SET_DOCK_HEIGHT[] = {42, 2, 2, 2, 0, 0, 0, 0, 0, 0};
 const uint8_t YOUR_DOCK_HAS_FULLFILLED_YOUR_REQUEST[] = {43, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+const uint8_t SERVO_ANGLE[] = {44, 2, 1, 1, 0, 0, 0, 0, 0, 0};
+const uint8_t STEPPER_PARAM[] = {45, 3, 1, 1, 1, 0, 0, 0, 0, 0};
+const uint8_t STEPPER_NB_STEPS[] = {46, 1, 2, 0, 0, 0, 0, 0, 0, 0};
+const uint8_t ENDSTEP[] = {47, 1, 2, 0, 0, 0, 0, 0, 0, 0};
 const uint8_t FRAMES[][10] = {
 	{0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
 	{1, 2, 1, 1, 0, 0, 0, 0, 0, 0},
@@ -38,19 +40,24 @@ const uint8_t FRAMES[][10] = {
 	{12, 3, 2, 2, 2, 0, 0, 0, 0, 0},
 	{13, 3, 2, 2, 2, 0, 0, 0, 0, 0},
 	{14, 3, 2, 2, 2, 0, 0, 0, 0, 0},
-	{15, 2, 2, 2, 0, 0, 0, 0, 0, 0},
+	{15, 3, 2, 2, 2, 0, 0, 0, 0, 0},
 	{16, 3, 2, 2, 2, 0, 0, 0, 0, 0},
 	{17, 3, 2, 2, 2, 0, 0, 0, 0, 0},
 	{18, 3, 2, 2, 2, 0, 0, 0, 0, 0},
 	{19, 2, 2, 2, 0, 0, 0, 0, 0, 0},
 	{20, 3, 2, 2, 2, 0, 0, 0, 0, 0},
-	{23, 4, 1, 1, 1, 1, 0, 0, 0, 0},
+	{23, 6, 1, 1, 1, 1, 1, 1, 0, 0},
 	{26, 0, 0, 0, 0, 0, 0, 0, 0, 0},
 	{28, 0, 0, 0, 0, 0, 0, 0, 0, 0},
 	{30, 2, 2, 1, 0, 0, 0, 0, 0, 0},
 	{40, 0, 0, 0, 0, 0, 0, 0, 0, 0},
 	{41, 0, 0, 0, 0, 0, 0, 0, 0, 0},
 	{42, 2, 2, 2, 0, 0, 0, 0, 0, 0},
-	{43, 0, 0, 0, 0, 0, 0, 0, 0, 0}};
-const int FRAMES_LENGTH = 25;
+	{43, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+	{44, 2, 1, 1, 0, 0, 0, 0, 0, 0},
+	{45, 3, 1, 1, 1, 0, 0, 0, 0, 0},
+	{46, 1, 2, 0, 0, 0, 0, 0, 0, 0},
+	{47, 1, 2, 0, 0, 0, 0, 0, 0, 0}
+};
+const int FRAMES_LENGTH = 29;
 // END OF PROTOCOL
